d9c0ceb13064165e9eb716fbe79f1fb1711d095f
Wojciech Kostecki <wt.kostecki@gmail.com>
2020-04-03 19:17:34 +0200

    Creating method in the classic version

---
+++ src/ClassicVersion.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClassicVersion {
+    public static void fizzBuzzClassic() {
+        List<Integer> numbers = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            numbers.add(i + 1);
+        }
+
+        for (Integer number : numbers) {
+            if (number % 3 == 0) {
+                System.out.println(number + " Fizz");
+            }
+            if (number % 5 == 0) {
+                System.out.println(number + " Buzz");
+            }
+            if (number % 3 == 0 && number % 5 == 0) {
+                System.out.println(number + " FizzBuzz");
+            }
+        }
+    }
+}
