0f19e42a606b2e2bea187d3a4d31a7d3ff3fcb99
Wojciech Kostecki <wt.kostecki@gmail.com>
2020-04-03 20:58:18 +0200

    Creating a test class

--- src/StreamVersion.java
+++ src/StreamVersion.java
@@ -1,3 +1,10 @@
+import java.util.stream.IntStream;
+
 public class StreamVersion {
-    
+    public static void fizzBuzzStream() {
+        IntStream.rangeClosed(1, 100)
+                .mapToObj(x -> x % 3 == 0 ? (x % 5 == 0 ? x + " FizzBuzz" : x + " Fizz") : (x % 5 == 0 ? x + " Buzz" : x))
+                .forEach(System.out::println);
+
+    }
 }
